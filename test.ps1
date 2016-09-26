c1af3a8f3092226153c3fa3a893c7bb47845aaa2
Manmeet Gill <contact@manmeetgill.com>
2016-09-26 17:42:22 +1000

    fix unit testing

--- test.ps1
+++ test.ps1
@@ -1,3 +1,3 @@
-﻿Start-Process powershell.exe -ArgumentList "jekyll build --watch" -Windowstyle Hidden
+﻿Start-Process powershell.exe -ArgumentList "jekyll build --watch"
 cd .\_site\
 live-server --port=4000 --browser="%PROGRAMFILES(X86)%\Firefox Developer Edition\firefox.exe"
