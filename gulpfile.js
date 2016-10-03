3025be3b7e94854041b5c0ab2241a4849b0a90af
Manmeet Gill <contact@manmeetgill.com>
2016-10-03 16:37:30 +1100

    Migrate to gulp for testing

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+﻿const gulp = require('gulp');
+const child = require('child_process');
+const browserSync = require('browser-sync').create();
+const gutil = require('gulp-util');
+const jekyllCommand = process.platform === "win32" ? "jekyll.bat" : "jekyll";
+const siteRoot = '_site';
+
+gulp.task('jekyll', () => {
+    const jekyll = child.spawn(jekyllCommand, ['build',
+        '--watch',
+        '--incremental',
+        '--drafts'
+    ]);
+
+    const jekyllLogger = (buffer) => {
+    buffer.toString()
+        .split(/\n/)
+        .forEach((message) => gutil.log('Jekyll: ' + message));
+    };
+
+    jekyll.stdout.on('data', jekyllLogger);
+    jekyll.stderr.on('data', jekyllLogger);
+});
+
+gulp.task('serve', () => {
+    browserSync.init({
+        files: [siteRoot + '/**'],
+        port: 4000,
+        server: {
+        baseDir: siteRoot
+        }
+    });
+});
+
+gulp.task('default', ['jekyll', 'serve']);
--- test.ps1
+++
@@ -1,3 +0,0 @@
-﻿Start-Process powershell.exe -ArgumentList "jekyll build --watch"
-cd .\_site\
-live-server --port=4000 --browser="%PROGRAMFILES(X86)%\Firefox Developer Edition\firefox.exe"
