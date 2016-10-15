03b8b376410a7fdfad675a9b634d67883c1dcb06
Manmeet Gill <contact@manmeetgill.com>
2016-10-15 20:55:26 +1100

    Add comments to ci script

--- travis.rb
+++ travis.rb
@@ -10,10 +10,11 @@ def cyan;           "\e[36m#{self}\e[0m" end
 def green;          "\e[32m#{self}\e[0m" end
 end
 
+# Set the URL to check from the first command line argument
 urlToCheck = ARGV[0]
 
+# Validate using W3C
 results = @validator.validate_uri(urlToCheck)
-
 if results.errors.length > 0
   results.errors.each do |err|
     puts err.to_s
@@ -23,4 +24,5 @@ else
   puts 'Checking links...'.green
 end
 
+# Validate using HTMLProofer
 HTMLProofer.check_links([urlToCheck]).run
