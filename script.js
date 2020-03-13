7bdb22a79fe376007daeaa8926cf362c32cee322
Hannah Ivy <contact@hannahi.com>
2020-03-13 19:09:41 +1100

    nothin

---
+++ script.js
@@ -0,0 +1,9 @@
+const now = new Date()
+const day = new Date(2020, 2, 16, 16)
+
+if (now >= day) {
+    items = document.getElementsByClassName("sn")
+    do {
+        items[0].classList.remove("sn")
+    } while (items.length > 0)
+}
